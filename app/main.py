c3d9cc1d24636e548847fb57d8cb11ef1f0cf9d5
Varma Kakalapudi <kVarma@aureustechsystems.com>
2020-03-07 12:36:55 +0530

    add change

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/api_3/<inp>')
+def api_3(inp):
+    analyser = SentimentIntensityAnalyzer()
+    score = analyser.polarity_scores(inp)
+    return("{:-<40} {}".format(inp, str(score)))
+    #return inp
+    #return inp
 
 if __name__ == '__main__':
   app.run()
